c1819f12bba3ef7cbeaead6688bc99e440a52213
Yun <531855995@qq.com>
2022-05-12 12:05:16 +0800

    Leecode_1047

--- src/main/java/Leecode_1047.java
+++ src/main/java/Leecode_1047.java
@@ -1,8 +1,25 @@
+import java.util.Stack;
+
 public class Leecode_1047 {
     public static void main(String[] args) {
-
+        String s = "abbbabaaa";
+        System.out.println(removeDuplicates(s));
     }
-    public static String removeDuplicates(String s) {
 
+    public static String removeDuplicates(String s) {
+        Stack stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (stack.isEmpty() || !stack.peek().equals(ch)) {
+                stack.push(ch);
+            } else {
+                stack.pop();
+            }
+        }
+        String str = "";
+        while (!stack.isEmpty()){
+            str = stack.pop() + str;
+        }
+        return str;
     }
 }
