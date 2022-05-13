a9f3f3a7a94195ef1c603d3e64090ffc8b04b34b
Yun <531855995@qq.com>
2022-05-13 20:39:54 +0800

    leecode_347lambda表达式写法探究

--- src/main/java/Leecode_347.java
+++ src/main/java/Leecode_347.java
@@ -12,9 +12,10 @@ public class Leecode_347 {
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        //
+        // map.entrySet()将map转换为key=value的集合，可以获取其key或者value
         Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
         System.out.println(entries);
+        // 根据map的value值正序排，相当于一个小顶堆
         PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(((o1, o2) -> {return o1.getValue() - o2.getValue();}));
         for (Map.Entry<Integer, Integer> entry : entries) {
             queue.offer(entry);
